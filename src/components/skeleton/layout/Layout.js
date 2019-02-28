1b2319058515c9dc92a6ad9ab37e0cdb6f6c1dfe
Matheus Paiva <matheus.paiva@hexis-hub.com>
2019-02-28 11:17:44 +0000

    feat: Add breadcrumb

--- src/components/skeleton/layout/Layout.js
+++ src/components/skeleton/layout/Layout.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Layout as AntLayout } from 'antd';
 import Toggle from '@render-props/toggle';
-import { renderRoutes } from 'react-router-config';
+import { renderRoutes, matchRoutes } from 'react-router-config';
 import { Footer, Sider, Header, Content } from '..';
 import * as menuTypes from '../../../routes/menuTypes';
 
 const Layout = ({ route, location }) => {
   const { routes } = route;
+  const { pathname } = location;
   const children = renderRoutes(routes);
-  const mainMenuRoutes = menuTypes.filterByType(routes, menuTypes.main)
+  const mainMenuRoutes = menuTypes.filterByType(routes, menuTypes.main);
+  const matchedRoutes = matchRoutes(routes, pathname);
 
   // In the future this info will come from .env
   const footer = 'Ant Design ©2018 Gapless-Tech';
@@ -17,10 +19,14 @@ const Layout = ({ route, location }) => {
     <Toggle initialValue={false}>
       {({ toggle, value }) => (
         <AntLayout style={{ minHeight: '100vh' }}>
-          <Sider isCollapsed={value} mainMenuRoutes={mainMenuRoutes} currentPath={location.pathname} />
+          <Sider
+            isCollapsed={value}
+            mainMenuRoutes={mainMenuRoutes}
+            matchedRoutes={matchedRoutes}
+          />
           <AntLayout>
             <Header isSiderCollapsed={value} onToggleSider={toggle} />
-            <Content>{children}</Content>
+            <Content matchedRoutes={matchedRoutes}>{children}</Content>
             <Footer footer={footer} />
           </AntLayout>
         </AntLayout>
