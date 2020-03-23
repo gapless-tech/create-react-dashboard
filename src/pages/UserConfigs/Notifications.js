fbd86b438572a630502fbf80173eeafc8d167469
codefactor-io <support@codefactor.io>
2020-03-23 06:12:41 +0000

    [CodeFactor] Apply fixes to commit 7d960e7

--- src/pages/UserConfigs/Notifications.js
+++ src/pages/UserConfigs/Notifications.js
@@ -26,7 +26,7 @@ class NotificationView extends Component {
 
   render() {
     return (
-      <Fragment>
+      <>
         <h2>Notifications</h2>
         <List
           itemLayout="horizontal"
@@ -37,7 +37,7 @@ class NotificationView extends Component {
             </List.Item>
           )}
         />
-      </Fragment>
+      </>
     );
   }
 }
